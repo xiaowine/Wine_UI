79cbb8b2fce7dc7922d7f13700a630024d58fa24
xiao_wine <xiaowine0@gmail.com>
2025-02-06 19:57:44 +0800

    将ThemeTransition样式独立出来

--- src/components/ThemeTransition/ThemeTransition.vue
+++ src/components/ThemeTransition/ThemeTransition.vue
@@ -54,39 +54,5 @@ defineExpose({
 </script>
 
 <style lang="scss" scoped>
-.theme-transition {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  pointer-events: none;
-  z-index: 9999;
-  visibility: hidden;
-
-  &::before {
-    content: "";
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    width: 300vmax;
-    height: 300vmax;
-    background-color: var(--next-theme-color);
-    transform-origin: center;
-    transform: translate(-50%, -50%) scale(0);
-    border-radius: 50%;
-    opacity: 0;
-    transition: transform 0.5s cubic-bezier(0.4, 0, 0.2, 1),
-      opacity 0.5s cubic-bezier(0.4, 0, 0.2, 1);
-  }
-
-  &--active {
-    visibility: visible;
-
-    &::before {
-      transform: translate(-50%, -50%) scale(1);
-      opacity: 1;
-    }
-  }
-}
+@use "./index.scss";
 </style>
